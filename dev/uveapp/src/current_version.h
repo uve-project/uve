52855cfe8a6d0ec803508cdb634cc3dc8fe054f2
Yann Thoma <yann.thoma@heig-vd.ch>
2013-12-28 15:33:46 +0100

    Updating the version

--- dev/uveapp/src/current_version.h
+++ dev/uveapp/src/current_version.h
@@ -37,6 +37,6 @@
   * - DD being the day.
   * - x being used to number the releases of a single day.
   */
-#define UVE_VERSION "13.03.12.1"
+#define UVE_VERSION "13.12.28.1"
 
 #endif // CURRENT_VERSION_H
